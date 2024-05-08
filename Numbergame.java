c707565c2536e7268ed43469e241a7076a16f8e1
Pawan Pathariya <113871795+Pawanpathariya@users.noreply.github.com>
2024-05-08 09:54:04 +0530

    Create Numbergame.java

---
+++ Numbergame.java
@@ -0,0 +1,78 @@
+import java.util.*;
+ 
+public class numbergame{
+ 
+    public static void
+    guessingNumberGame()
+    {
+      
+        try (Scanner sc = new Scanner(System.in)) {
+            int number = 1 + (int)(100
+                                   * Math.random());
+ 
+    
+            int K = 5;
+ 
+            int i, guess;
+ 
+            System.out.println(
+                "A number is chosen "+
+                "between 1 to 100"+
+                " Guess the number"+
+                " within 5 trials.");
+ 
+   
+            for (i = 0; i < K; i++) {
+ 
+                System.out.println(
+                    "Guess the number:");
+ 
+                // Take input for guessing
+                guess = sc.nextInt();
+    
+                // If the number is guessed
+                if (number == guess) {
+                    System.out.println(
+                        "Congratulations!"
+                        + " You guessed the number.");
+                    break;
+                }
+                else if (number > guess
+                         && i != K - 1) {
+                    System.out.println(
+                        "The number is "
+                        + "greater than " + guess);
+                }
+                else if (number < guess
+                         && i != K - 1) {
+                    System.out.println(
+                        "The number is"
+                        + " less than " + guess);
+                }
+            
+            }
+
+            if (i == K) {
+                System.out.println(
+                    "You have exhausted "
+                    + K + "  trials.");
+ 
+                System.out.println(
+                    "The number was " + number);
+                       
+            }
+      sc.close();
+        }
+    }
+ 
+   
+    public static void
+    main(String arg[])
+    {
+ 
+       
+        guessingNumberGame();
+         
+    }
+
+}
